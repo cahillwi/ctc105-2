c2967e538922793773d8da00419a9b2ad11ab1f4
William Cahill <williamcahill@Williams-MacBook-Pro.local>
2021-06-15 07:03:37 -0400

    Auth middleware

--- users/services/users.service.ts
+++ users/services/users.service.ts
@@ -32,6 +32,9 @@ class UsersService implements CRUD {
     async getUserByEmail(email: string) {
         return UsersDao.getUserByEmail(email);
     }
+    async getUserByEmailWithPassword(email: string) {
+        return UsersDao.getUserByEmailWithPassword(email);
+    }
 }
 
 export default new UsersService();
